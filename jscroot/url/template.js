a168b0b3d4b06d2b955d7dc79a786444f67e47db
GilarYa <gilarwahibul@gmail.com>
2024-02-02 00:52:16 +0700

    pushh

---
+++ jscroot/url/template.js
@@ -0,0 +1,16 @@
+export let URLGeoJson = "https://asia-southeast2-gilartest.cloudfunctions.net/getdatayaw";
+export let urlPostGCF = "https://asia-southeast2-gilartest.cloudfunctions.net/larch3v2";
+export let tableTag="tr";
+export let tableRowClass="content is-small";
+export let tableTemplate=`
+<td>#NAME#</td>
+<td >#KORDINAT#</td>
+<td>#TYPE#</td>
+`
+export const clickpopup = `
+Long : #LONG#<br>
+Lat  : #LAT#<br>
+X   : #X#<br>
+Y   : #Y#<br>
+HDMS : #HDMS#<br>
+`
